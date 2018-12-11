79090327415fcdf99987e33cb4b5377cb1f5c3f7
EC2 Default User <ec2-user@ip-172-31-35-191.ap-northeast-1.compute.internal>
2018-12-11 08:42:16 +0000

    test

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'test');
+define('DB_USER', 'root');
+define('DB_PASS', '0221');
+define('DB_CHARSET', 'utf8');
